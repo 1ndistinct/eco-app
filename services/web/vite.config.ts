56804a62ba9bb3a84c6ec17891a357e2d3c61ae3
Ciaran Mckey <ciaran.mckey@Ciarans-MacBook-Pro-2.local>
2026-04-29 01:30:35 +0100

    visual tweak

--- services/web/vite.config.ts
+++ services/web/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(() => {
       !isTest &&
         federation({
           name: "shellApp",
+          hostInitInjectLocation: "entry",
           remotes: {
             todoApp: {
               type: "module",
